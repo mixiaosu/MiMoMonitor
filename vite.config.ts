2bdcb2e966baa1b30c162ed9c71786dfb50f0fae
mixiaosu <justicebeatty97@gmail.com>
2026-06-08 17:47:59 +0800

    fix: vite base relative path to resolve WebView blank screen

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./",
   plugins: [react()],
+  build: {
+    modulePreload: { polyfill: false },
+  },
   server: {
     port: 5181,
     strictPort: false,
